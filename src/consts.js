581b6d3138b699ae1c9ef81d13c09ae0523f77ad
dangxingjie <1328791209@qq.com>
2025-01-10 19:28:14 +0800

    feat:换hexo

--- src/consts.js
+++
@@ -1,5 +0,0 @@
-export const SITE_TITLE = `DxJay's Blog`
-export const SITE_DESCRIPTION = 'DxJay Site Description'
-export const SITE_EMAIL = ''
-export const SITE_NAME = ''
-export const SITE_URL = 'https://astro.yufengbiji.com'
